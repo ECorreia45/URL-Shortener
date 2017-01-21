090b0ea8a30e48da65dd29d470f2069ed980c3ec
ecorreia design <el4591@hotmail.com>
2017-01-21 03:43:19 -0500

    hhh

--- urlShortApi/routes/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(express) {
-    const router = express.Router();
-
-    router.use('/', require('./api/url')(express));
-    router.use('/', require('./api/crud')(express));
-    router.use('/', require('./api/go')(express));
-
-    return router;
-};
