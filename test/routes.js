c06291811baf775d1b48d4eadbddaba53c694fd9
ecorreia design <el4591@hotmail.com>
2017-01-22 21:19:32 -0500

    temp tests changes

--- test/routes.js
+++ test/routes.js
@@ -22,29 +22,29 @@ describe('Server', () => {
     });
   });
   // Test CRUD capabilities
-  describe('CRUD', () => {
-    // Test posting the data to the DB
-    it('Post through /api/v1/url/:url just fine', (done) => {
-      app.get('/api/v1/url/elsoncorreia.com').expect(200).end(done);
-    });
-    // Test updating one data on DB
-    it('Update through /api/v1/urls/1 just fine', (done) => {
-      app.post('/api/v1/urls/1').set('Content-Type','application/json')
-        .send({ id: '2', url: 'unittest.com', shortURL: '/go/uni00ezg' })
-        .expect(200)
-        .end(done);
-    });
-    // Test getting one data from DB
-    it('Get url through /api/v1/urls/2 just fine', (done) => {
-      app.get('/api/v1/urls/2').expect(200).end(done);
-    });
-    // Test if all the data is returned
-    it('Get all data through /api/v1/urls just fine', (done) => {
-      app.get('/api/v1/urls').expect(200).end(done);
-    });
-    // Test deleting one data from DB
-    it('Delete through /api/v1/urls/2 just fine', (done) => {
-      app.delete('/api/v1/urls/2').expect(200).end(done);
-    });
-  });
+  // describe('CRUD', () => {
+  //   // Test posting the data to the DB
+  //   it('Post through /api/v1/url/:url just fine', (done) => {
+  //     app.get('/api/v1/url/elsoncorreia.com').expect(200).end(done);
+  //   });
+  //   //Test updating one data on DB
+  //   it('Update through /api/v1/urls/1 just fine', (done) => {
+  //     app.post('/api/v1/urls/1').set('Content-Type','application/json')
+  //       .send({ id: '1', url: 'unittest.com', shortURL: '/go/uni00ezg' })
+  //       .expect(200)
+  //       .end(done);
+  //   });
+  //   // Test getting one data from DB
+  //   it('Get url through /api/v1/urls/2 just fine', (done) => {
+  //     app.get('/api/v1/urls/2').expect(200).end(done);
+  //   });
+  //   // Test if all the data is returned
+  //   it('Get all data through /api/v1/urls just fine', (done) => {
+  //     app.get('/api/v1/urls').expect(200).end(done);
+  //   });
+  //   // Test deleting one data from DB
+  //   it('Delete through /api/v1/urls/2 just fine', (done) => {
+  //     app.delete('/api/v1/urls/2').expect(200).end(done);
+  //   });
+  // });
 });
